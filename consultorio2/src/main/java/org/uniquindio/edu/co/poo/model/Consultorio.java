b8d691e661129744663dae99231e9e2d4f937cfe
JeronimoFernandezR <jeronimo.fernandezr@uqvirtual.edu.co>
2025-03-30 15:50:49 -0500

    Update Consultorio.java

--- consultorio2/src/main/java/org/uniquindio/edu/co/poo/model/Consultorio.java
+++ consultorio2/src/main/java/org/uniquindio/edu/co/poo/model/Consultorio.java
@@ -5,4 +5,39 @@ import java.util.List;
 
 public record Consultorio(String nombre, List<Paciente> listaPacientes, String direccion, ArrayList<Odontologo> listaOdontologos) {
 
+    // crud para odontologo
+    public Consultorio agregarOdontologo(Odontologo odontologo) {
+        ArrayList<Odontologo> nuevaLista = new ArrayList<>(listaOdontologos);
+        nuevaLista.add(odontologo);
+        return new Consultorio(nombre, listaPacientes, direccion, nuevaLista);
+    }
+
+    
+    public Odontologo obtenerOdontologoPorId(String id) {
+        for (Odontologo odontologo : listaOdontologos) {
+            if (odontologo.getId().equals(id)) {
+                return odontologo;
+            }
+        }
+        return null; 
+    }
+
+    
+    public Consultorio actualizarOdontologo(String id, Odontologo odontologoActualizado) {
+        ArrayList<Odontologo> nuevaLista = new ArrayList<>(listaOdontologos);
+        for (int i = 0; i < nuevaLista.size(); i++) {
+            if (nuevaLista.get(i).getId().equals(id)) {
+                nuevaLista.set(i, odontologoActualizado);
+                return new Consultorio(nombre, listaPacientes, direccion, nuevaLista);
+            }
+        }
+        return this; 
+    }
+
+   
+    public Consultorio eliminarOdontologo(String id) {
+        ArrayList<Odontologo> nuevaLista = new ArrayList<>(listaOdontologos);
+        nuevaLista.removeIf(odontologo -> odontologo.getId().equals(id));
+        return new Consultorio(nombre, listaPacientes, direccion, nuevaLista);
+    }
 }
